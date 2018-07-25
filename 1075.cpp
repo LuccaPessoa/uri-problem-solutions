ea4f49eeede5b856ce6c8e5b2c8936c79996a61c
LuccaPessoa <luccapsm@gmail.com>
2018-07-24 23:19:32 -0300

    Create 1075.cpp

---
+++ 1075.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	int n = 0;
+	cin >> n; //lendo a variável N
+  
+	//regra do negócio
+	for(int i = 1; i < 1000; i++){
+		
+		if(i % n == 2){
+			cout << "\n" << i;
+		}
+	}
+	return 0;
+  }
