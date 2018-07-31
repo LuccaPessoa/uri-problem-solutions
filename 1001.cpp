c87006f905aead9906eac2a7b8742816835deef9
Lucca Pessoa <luccapsm@gmail.com>
2018-07-31 17:26:31 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int soma(int numero_1, int numero_2){
+	
+	return numero_1 + numero_2;
+}
+
+int main(){
+	
+	int A, B;
+	cin >> A;
+	cin >> B;
+	
+	cout << "X = " << soma(A, B) << endl;
+	
+	return 0;
+}
